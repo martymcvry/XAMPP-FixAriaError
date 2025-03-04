2e471048ba72f87d94060dea8d7a0669b98a9a08
Marty McVry <m.verstraelen@gmail.com>
2025-03-04 14:00:27 +0100

    Add project files.

---
+++ XAMPP-FixAriaError/frmAriaRepair.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace XAMPP_FixAriaError
+{
+    partial class frmAriaRepair
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMain1 = new System.Windows.Forms.Label();
+            this.lblXAMPPv1 = new System.Windows.Forms.Label();
+            this.lblXAMPPVersion = new System.Windows.Forms.Label();
+            this.lblXAMPPl1 = new System.Windows.Forms.Label();
+            this.lblXAMPPLocation = new System.Windows.Forms.Label();
+            this.btnEditLocation = new System.Windows.Forms.Button();
+            this.btnRepair = new System.Windows.Forms.Button();
+            this.fbdInstallFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // lblMain1
+            // 
+            this.lblMain1.AutoSize = true;
+            this.lblMain1.Location = new System.Drawing.Point(15, 11);
+            this.lblMain1.Name = "lblMain1";
+            this.lblMain1.Size = new System.Drawing.Size(590, 48);
+            this.lblMain1.TabIndex = 0;
+            this.lblMain1.Text = "Op de één of andere vreemde manier loopt het mis bij de opstart van MySQL (MariaD" +
+    "B) in XAMPP.\r\n\r\nDit programma biedt mogelijk een oplossing.";
+            // 
+            // lblXAMPPv1
+            // 
+            this.lblXAMPPv1.AutoSize = true;
+            this.lblXAMPPv1.Location = new System.Drawing.Point(15, 102);
+            this.lblXAMPPv1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblXAMPPv1.Name = "lblXAMPPv1";
+            this.lblXAMPPv1.Size = new System.Drawing.Size(191, 16);
+            this.lblXAMPPv1.TabIndex = 1;
+            this.lblXAMPPv1.Text = "Gedetecteerde XAMPP-versie:";
+            // 
+            // lblXAMPPVersion
+            // 
+            this.lblXAMPPVersion.AutoSize = true;
+            this.lblXAMPPVersion.Location = new System.Drawing.Point(263, 102);
+            this.lblXAMPPVersion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblXAMPPVersion.Name = "lblXAMPPVersion";
+            this.lblXAMPPVersion.Size = new System.Drawing.Size(79, 16);
+            this.lblXAMPPVersion.TabIndex = 2;
+            this.lblXAMPPVersion.Text = "<<version>>";
+            // 
+            // lblXAMPPl1
+            // 
+            this.lblXAMPPl1.AutoSize = true;
+            this.lblXAMPPl1.Location = new System.Drawing.Point(16, 132);
+            this.lblXAMPPl1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblXAMPPl1.Name = "lblXAMPPl1";
+            this.lblXAMPPl1.Size = new System.Drawing.Size(194, 16);
+            this.lblXAMPPl1.TabIndex = 3;
+            this.lblXAMPPl1.Text = "Gedetecteerde XAMPP-locatie:";
+            // 
+            // lblXAMPPLocation
+            // 
+            this.lblXAMPPLocation.AutoSize = true;
+            this.lblXAMPPLocation.Location = new System.Drawing.Point(263, 132);
+            this.lblXAMPPLocation.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblXAMPPLocation.Name = "lblXAMPPLocation";
+            this.lblXAMPPLocation.Size = new System.Drawing.Size(82, 16);
+            this.lblXAMPPLocation.TabIndex = 4;
+            this.lblXAMPPLocation.Text = "<<location>>";
+            // 
+            // btnEditLocation
+            // 
+            this.btnEditLocation.Location = new System.Drawing.Point(600, 119);
+            this.btnEditLocation.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEditLocation.Name = "btnEditLocation";
+            this.btnEditLocation.Size = new System.Drawing.Size(43, 39);
+            this.btnEditLocation.TabIndex = 5;
+            this.btnEditLocation.TabStop = false;
+            this.btnEditLocation.Text = "✏️";
+            this.btnEditLocation.UseVisualStyleBackColor = true;
+            this.btnEditLocation.Click += new System.EventHandler(this.btnEditLocation_Click);
+            // 
+            // btnRepair
+            // 
+            this.btnRepair.Enabled = false;
+            this.btnRepair.Location = new System.Drawing.Point(231, 276);
+            this.btnRepair.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRepair.Name = "btnRepair";
+            this.btnRepair.Size = new System.Drawing.Size(267, 62);
+            this.btnRepair.TabIndex = 6;
+            this.btnRepair.Text = "Repareer de Aria-fout";
+            this.btnRepair.UseVisualStyleBackColor = true;
+            this.btnRepair.Click += new System.EventHandler(this.btnRepair_Click);
+            // 
+            // fbdInstallFolder
+            // 
+            this.fbdInstallFolder.Description = "Selecteer de map waarin xampp-control.exe zich bevindt. Dit is de hoofdmap van de" +
+    " XAMPP-installatie.";
+            this.fbdInstallFolder.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.fbdInstallFolder.SelectedPath = "C:\\";
+            this.fbdInstallFolder.ShowNewFolderButton = false;
+            this.fbdInstallFolder.HelpRequest += new System.EventHandler(this.fbdInstallFolder_HelpRequest);
+            // 
+            // frmAriaRepair
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 432);
+            this.Controls.Add(this.btnRepair);
+            this.Controls.Add(this.btnEditLocation);
+            this.Controls.Add(this.lblXAMPPLocation);
+            this.Controls.Add(this.lblXAMPPl1);
+            this.Controls.Add(this.lblXAMPPVersion);
+            this.Controls.Add(this.lblXAMPPv1);
+            this.Controls.Add(this.lblMain1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(726, 479);
+            this.MinimumSize = new System.Drawing.Size(726, 479);
+            this.Name = "frmAriaRepair";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XAMPP - Repareer de \'aria\'-fout";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frmAriaRepair_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMain1;
+        private System.Windows.Forms.Label lblXAMPPv1;
+        private System.Windows.Forms.Label lblXAMPPVersion;
+        private System.Windows.Forms.Label lblXAMPPl1;
+        private System.Windows.Forms.Label lblXAMPPLocation;
+        private System.Windows.Forms.Button btnEditLocation;
+        private System.Windows.Forms.Button btnRepair;
+        private System.Windows.Forms.FolderBrowserDialog fbdInstallFolder;
+    }
+}
+
---
+++ XAMPP-FixAriaError/frmAriaRepair.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Win32;
+using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+using System.Diagnostics;
+//using System.Drawing;
+using System.IO;
+using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XAMPP_FixAriaError
+{
+    public partial class frmAriaRepair : Form
+    {
+        // 2 variabelen aanmaken voor controle geldigheid installatie
+        private bool validInstallFound = false;
+        private string validInstallLocation = "";
+        public frmAriaRepair()
+        {
+            InitializeComponent();
+        }
+        private void checkInstallLocation(string installLocation, bool showMessage = true)
+        {
+            string uninstallLocation = installLocation + "\\uninstall.exe";
+            string controlLocation = installLocation + "\\xampp-control.exe";
+            lblXAMPPLocation.Text = installLocation;
+            if (!File.Exists(controlLocation))
+            {
+                // Ongeldige installatie!
+                validInstallFound = false;
+                validInstallLocation = "";
+                btnRepair.Enabled = false;
+                lblXAMPPLocation.ForeColor = System.Drawing.Color.Red;
+                btnEditLocation.Enabled = true;
+                btnEditLocation.Visible = true;
+                lblXAMPPVersion.Text = "<< niet gevonden >>";
+                if (showMessage)
+                {
+                    MessageBox.Show("Geen geldige installatie gevonden!\nHeb je XAMPP verplaatst?\n\nKlik dadelijk op ✏️ om de juiste locatie van xampp-control.exe te kiezen.", "Kies een map", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            else
+            {
+                // Geldige installatie!
+                validInstallFound = true;
+                validInstallLocation = installLocation;
+                RegistryKey fixKey = Registry.CurrentUser.CreateSubKey(@"Software\MartyMcVry\XAMPPFix");
+                fixKey.SetValue("InstallLocation", installLocation);
+                btnRepair.Enabled = true;
+                lblXAMPPLocation.ForeColor = System.Drawing.Color.Black;
+                btnEditLocation.Enabled = false;
+                btnEditLocation.Visible = false;
+                if (File.Exists(uninstallLocation))
+                {
+                    lblXAMPPVersion.Text = FileVersionInfo.GetVersionInfo(uninstallLocation).ProductVersion;
+                }
+                else
+                {
+                    lblXAMPPVersion.Text = "<< kan versie niet ophalen >>";
+                }
+            }
+        }
+        private void frmAriaRepair_Load(object sender, EventArgs e)
+        {
+            // Eerst kijken we in de registersleutel van dit programma
+            RegistryKey fixKey = Registry.CurrentUser.OpenSubKey(@"Software\MartyMcVry\XAMPPFix");
+            // Er is ook de sleutel van XAMPP zelf
+            // 64bit modus! (compileren met 32bits geeft een onleesbare sleutel)
+            RegistryKey xamppKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\xampp");
+
+            // Nonsens-waarde voor locatie, die sowieso een foutmelding zal geven
+            string checkLocation = @"M:\DezeMapBestaatNietTenzijJeDieManueelHebtGemaaktMaarDatIsCompleetNutteloos";
+            lblXAMPPVersion.Text = "<< niet gevonden >>";
+            lblXAMPPLocation.Text = "<< niet gevonden >>";
+
+            // Cascade: eerst kijken in eigen sleutel
+            if (fixKey != null)
+            {
+                // XAMPP kan verplaatst zijn sinds de vorige keer;
+                // installatie nog verifiëren ZONDER foutmelding (bool showMessage = false)
+                // want er volgt hoe dan ook een tweede controle als deze niet slaagt
+                checkLocation = fixKey.GetValue("InstallLocation").ToString();
+                checkInstallLocation(checkLocation, false);
+            }
+
+            // Als er nog geen geldige installatie gevonden is, ga verder
+            // Dit stukje wordt dus ook uitgevoerd als de eigen sleutel niet gevonden is (fixKey == null).
+            if (!validInstallFound)
+            {
+                // Maar als de XAMPP-sleutel toch gevonden is, controleer die sleutel
+                if (xamppKey != null)
+                {
+                    checkLocation = xamppKey.GetValue("InstallLocation").ToString();
+                }
+
+                // Wat is op dit punt de waarde van checkLocation?
+                // - fixKey gevonden? --> locatie uit register van dit programma, maar geen probleem want
+                //                        als die zou bestaan, zou dit stukje code niet worden uitgevoerd
+                // - fixKey niet gevonden maar wel de xamppKey? --> locatie uit register van XAMPP
+                // - fixKey niet gevonden en xamppKey niet gevonden? --> nonsens-locatie van eerder
+                // Dit keer wél met foutmelding
+                checkInstallLocation(checkLocation);
+            }
+        }
+
+        private void btnEditLocation_Click(object sender, EventArgs e)
+        {
+            // Toon dialoogvenster en kijk of op "ok" werd geklikt
+            if (fbdInstallFolder.ShowDialog() == DialogResult.OK)
+            {
+                // controleer dan de locatie en geef indien nodig foutmelding
+                checkInstallLocation(fbdInstallFolder.SelectedPath);
+            }
+        }
+
+        private void fbdInstallFolder_HelpRequest(object sender, EventArgs e)
+        {
+            MessageBox.Show("You're on your own, bro. Good luck!", "T.U.P.");
+        }
+
+        private void btnRepair_Click(object sender, EventArgs e)
+        {
+            string folder = validInstallLocation + "\\mysql\\data";
+            // Controleer of de map bestaat
+            if (Directory.Exists(folder))
+            {
+                // zoek naar de log-bestanden van ARIA
+                string[] logFiles = Directory.GetFiles(folder, "aria_log*");
+                MessageBox.Show(logFiles.Count().ToString() + " bestand(en) gevonden.","Bezig met reparatie...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                foreach (string logFilename in logFiles)
+                {
+                    File.Delete(logFilename);
+                }
+            }
+            else
+            {
+                MessageBox.Show("De map waarin de ARIA-bestanden moeten staan, bestaat niet.\nInstalleer XAMPP gewoon opnieuw, dat is het makkelijkste!", "Oeps...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // Programma mag gesloten worden nu
+            Application.Exit();
+        }
+    }
+}
+
